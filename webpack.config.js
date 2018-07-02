0ac2a33d59d70188cc022b365d7e82b298d46ae2
Sergey Titov <sergey.sergeevich.titov@protonmail.com>
2018-07-03 02:37:00 +0300

    chore: webpack configured

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+const App = require(./App);
+
+module.exports = {
+  context: __dirname,
+  entry: "./src/scrpt/ClientApp.jsx",
+  devtool: "cheap-eval-source-stash-map",
+  output: {
+    path: path.join(__dirname, "public"),
+    filename: "bundle.js"
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".json" ]
+  },
+  stats: {
+    colors: true,
+    reasons: true,
+    chunks: true
+  },
+  module:{
+    rules: {
+      test: /\.jsx?$/,
+      loader: "babel-loader"
+    }
+  }
+};
