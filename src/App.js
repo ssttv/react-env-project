413c25cd1a6fefd243bf388ce529184590da1748
Sergey Titov <sergey.sergeevich.titov@protonmail.com>
2018-07-08 23:46:47 +0300

    feat: init main router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import logo from "./logo.svg";
 import "./App.css";
 
 const multiply = (a, b) => a * b;
-const FourOhFour = () => <h1>404</h1>
+const FourOhFour = () => <h1>404</h1>;
 const App = () => (
   <div classname="app">
     <div classname="landing">
